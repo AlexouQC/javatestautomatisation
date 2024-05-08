72b5bef15559b4b2ab0e3448405bfeaf398937c9
Alexandre Dufresne <alexxxuo@gmail.com>
2024-05-08 19:14:15 -0400

    Commit v3 - Only for particing

--- src/automatisation_formation/testformation.java
+++ src/automatisation_formation/testformation.java
@@ -2,11 +2,22 @@ package automatisation_formation;
 
 public class testformation {
 	
+	int result=0;
+	
 	public static void main(String[] args ) {
-		int a = 0;
-		System.out.print(a);
+	
 				
 		
 	}
+	
+	
+	public int calculSomme(int number1 , int number2){
+		
+		result=number1+number2;
+		return result;
+		
+	}
+	
+	
 
 }
